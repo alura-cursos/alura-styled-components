e25f1ca7156a9df802797a03eb06a26204531cc0
elaine mattos <elaine.mattos@outlook.pt>
2020-10-06 19:37:25 -0300

    Variáveis e Cabeçalho

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import { corPrimaria } from "../UI/variaveis";
+
+const StyledCabecalho = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`;
 
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+    <StyledCabecalho>
+      <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
         <a className="btn-secundario" href="https://google.com">
           Ajuda
@@ -13,7 +29,7 @@ const Cabecalho = () => {
           Sair
         </a>
       </div>
-    </div>
+    </StyledCabecalho>
   );
 };
 
---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,3 @@
+export const corPrimaria = "#41d3be";
+export const fundoClaro = "#f1f1f1";
+export const textoFundoClaro = "grey";
