7a45f44d35ff1368bd56dcd5153a293ef09988b2
elaine mattos <elaine.mattos@outlook.pt>
2020-10-07 09:36:03 -0300

    Refatoraçao - Container e Conteudo

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
-const Container = () => {
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export default () => {
   return (
-    <div className="container">
+    <Container>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+      </Conteudo>
+    </Container>
   );
 };
-
-export default Container;
